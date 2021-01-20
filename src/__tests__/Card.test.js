3de584493832be523b98fb4ecbc0eb72caada10d
@jonathanlei <jonathanlei98@gmail.com>
2021-01-19 19:08:56 -0500

    finished part 1, pre code review

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -12,12 +12,20 @@ it("matches snapshot", function() {
   expect(container).toMatchSnapshot();
 });
 
+it("renders properly", function() {
+  const {container, debug} = render(<Card />);
+  debug(container);
+  expect(container.querySelector(".Card-title")).toContainHTML("Photo by Richard Pasquarella on Unsplash")
+  expect(container.querySelector(".Card-image").getAttribute("src")).toEqual(image1);
+  expect(container.querySelector(".Card-image").getAttribute("alt")).toEqual("Photo by Richard Pasquarella on Unsplash");
+  expect(container.querySelector(".Card-small")).toContainHTML("Image 1 of 1")
+})
 
 Card.defaultProps = 
     {
       src: image1,
       caption: "Photo by Richard Pasquarella on Unsplash",
-      currentNum: 1,
+      currNum: 1,
       totalNum: 1
     }
    
--- src/__tests__/Carousel.test.js
+++ src/__tests__/Carousel.test.js
@@ -26,3 +26,43 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
+
+
+
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+});
+
+
+
+it("arrows are missing when you are on the first or last image", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // expect left arrow missing
+  expect(queryByTestId("left-arrow")).not.toBeInTheDocument();
+  const rightArrow = queryByTestId("right-arrow");
+  // expect left arrow to appear
+  fireEvent.click(rightArrow);
+  expect(queryByTestId("left-arrow")).toBeInTheDocument();
+  
+  // expect right arrow missing, left arrow stil here
+  fireEvent.click(rightArrow);
+  expect(queryByTestId("right-arrow")).not.toBeInTheDocument();
+  expect(queryByTestId("left-arrow")).toBeInTheDocument();
+});
